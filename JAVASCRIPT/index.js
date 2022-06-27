c9941844f7f8d9af057e5cf9549cdc060a3ecdb2
Mateus Rocha <mateus046@gmail.com>
2022-06-27 17:59:24 -0300

    const

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,16 @@
+//comentário é feito dessa forma
+/*  ou
+    dessa
+    forma*/
+console.log('Meu nome é Mateus. Estou aprendendo a linguagem de programação Javascript.')
+
+const idade = 26
+const peso = 74
+const altura = 1.72
+let imc
+let anoNascimento
+
+imc = peso / (altura * altura)
+anoNascimento = 2022 - idade
+
+console.log(imc, anoNascimento)

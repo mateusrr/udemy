2b10060b726a5bac75bde81fc71732cd87fc27c2
Mateus Rocha <mateus046@gmail.com>
2022-06-28 14:31:25 -0300

    opTenario e obDate

---
+++ JAVASCRIPT/logica/operadorTernario.js
@@ -0,0 +1,5 @@
+// (condicao) ? 'valor para verdadeiro' : 'valor para falso'
+
+const pontuacaoUsuario = 800
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal'
+console.log(nivelUsuario)

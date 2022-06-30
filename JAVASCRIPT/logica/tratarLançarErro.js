253140a8e05f529bb9d01663a2e87bbd48960f9d
Mateus Rocha <mateus046@gmail.com>
2022-06-30 10:04:33 -0300

    tratarLancarErro

--- JAVASCRIPT/logica/tratarLançarErro.js
+++ JAVASCRIPT/logica/tratarLançarErro.js
@@ -13,3 +13,31 @@ try {
     //console.log(error)
     console.log('O que foi digitado não corrresponde a números, digita novamente.')
 }
+
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new Error('Esperando instância de Date.')
+    }
+
+    if (!data) {
+        data = new Date()
+    }
+
+    return data.toLocaleTimeString('pr-BR', 
+    {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    })
+}
+
+try {
+const data = new Date('01-01-1970 12:56:11')
+const hora = retornaHora()
+console.log(hora)
+} catch (e) {
+    //trate o erro
+} finally {
+    console.log('good morning')
+}

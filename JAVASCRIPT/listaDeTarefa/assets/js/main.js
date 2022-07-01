038346612efb8f7aca23113c7b42f9cc89254b67
Mateus Rocha <mateus046@gmail.com>
2022-07-01 10:47:28 -0300

    update coments

--- JAVASCRIPT/listaDeTarefa/assets/js/main.js
+++ JAVASCRIPT/listaDeTarefa/assets/js/main.js
@@ -1,24 +1,32 @@
+// Seleção das classes criadas no index.html
 const inputTarefa = document.querySelector('.input-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
 const tarefas = document.querySelector('.tarefas');
+// Fim
 
+// Função para retornar o Li
 function criaLi() {
     const li = document.createElement('li');
     return li;
 }
 
+// Evento para que a tecla Enter seja habilitada ao preencher o campo.
 inputTarefa.addEventListener('keypress', function(e) {
+    // 13 é o código da tecla Enter - console.log(e) - visto no Console
     if (e.keyCode === 13) {
       if (!inputTarefa.value) return;
       criaTarefa(inputTarefa.value);
     }
 });
-  
+
+// Após o preenchimento do campo e adicionado nova tarefa,
+// o campo é limpo pronto para adiciona uma nova tarefa
 function limpaInput() {
     inputTarefa.value = '';
     inputTarefa.focus();
   }
   
+// Apagar as tarefas já existentes
   function criaBotaoApagar(li) {
     li.innerText += ' ';
     const botaoApagar = document.createElement('button');
@@ -38,8 +46,10 @@ function criaTarefa(textoInput) {
     salvarTarefas();
   }
   
+  // Evento de clique na classe inputTarefa
   btnTarefa.addEventListener('click', function() {
     if (!inputTarefa.value) return;
+    // Mandando o texto para uma função criaTarefa
     criaTarefa(inputTarefa.value);
   });
   

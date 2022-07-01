04abc943d800e3b59c405a22d0a92de110803381
Mateus Rocha <mateus046@gmail.com>
2022-07-01 12:09:35 -0300

    retornoFunção

---
+++ JAVASCRIPT/functionAvanced/retornoFuncao.js
@@ -0,0 +1,29 @@
+// return
+// retorna um valor
+// termina a função
+
+function soma(a, b) {
+    return a + b
+}
+console.log(soma(2,5))
+
+function criaPessoa(nome, sobrenome) {
+    return {nome, sobrenome}
+}
+const p1 = criaPessoa('mateus', 'rocha')
+console.log(p1)
+
+// Retornando uma função dentro de outra função
+function criaMultiplicador(multiplicador) {
+    return function(n) {
+        return n * multiplicador
+    }
+}
+
+const duplica = criaMultiplicador(2)
+const triplica = criaMultiplicador(3)
+const quadruplica = criaMultiplicador(4)
+
+console.log('Duplicação:', duplica(3))
+console.log('Triplicação:', triplica(4))
+console.log('Quadruplicação:', quadruplica(5))

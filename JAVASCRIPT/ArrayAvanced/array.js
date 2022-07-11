34e0e717f068ab4a15e8cbc6274777a43b410d38
Mateus Rocha <mateus046@gmail.com>
2022-07-11 19:34:45 -0300

    update array

---
+++ JAVASCRIPT/ArrayAvanced/array.js
@@ -0,0 +1,12 @@
+//String em Array
+/*
+const nome = 'Silvio Silva Salvador'
+const nomes = nome.split(' ')
+console.log(nomes)
+console.log(nome)
+*/
+
+// Array em String
+const nomes = ['Silvio', 'Silva', 'Salvador']
+const nome = nomes.join(' ')
+console.log(nome)

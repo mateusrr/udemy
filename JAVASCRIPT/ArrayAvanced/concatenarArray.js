4b82a88e93b2c350cc64044f5c8c97d8d3e3e44d
Mateus Rocha <mateus046@gmail.com>
2022-07-12 14:21:25 -0300

    update array

---
+++ JAVASCRIPT/ArrayAvanced/concatenarArray.js
@@ -0,0 +1,11 @@
+/*const numeros1 = [1, 2, 3]
+const numeros2 = [4, 5, 6]
+
+const conjunto = numeros1.concat(numeros2)
+console.log(conjunto)*/
+
+// filter, map, reduce
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+const numerosFiltrados = numeros.filter(valor => valor > 10)
+console.log(numerosFiltrados)
---
+++ JAVASCRIPT/ArrayAvanced/splice.js
@@ -0,0 +1,8 @@
+//nomes.splice(índice atual, delete(qnt))
+const nomes = ['paulo', 'armando', 'marca', 'juana']
+
+//nomes.splice(2, 2)
+
+//push
+nomes.splice(4, 0, 'Marcos', 'Carlos')
+console.log(nomes)

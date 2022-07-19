c969b2bdef1ac0107d15673531716d3fbd48515d
Mateus Rocha <mateus046@gmail.com>
2022-07-19 09:11:01 -0300

    update

--- JAVASCRIPT/ArrayAvanced/concatenarArray.js
+++ JAVASCRIPT/ArrayAvanced/concatenarArray.js
@@ -8,4 +8,4 @@ console.log(conjunto)*/
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
 const numerosFiltrados = numeros.filter(valor => valor > 10)
-console.log(numerosFiltrados)
+console.log(numerosFiltradose)

4d9774aee64a48a5b8314e6ec07cb7a8391407b9
Mateus Rocha <mateus046@gmail.com>
2022-08-09 10:58:21 -0300

    update array

--- JAVASCRIPT/ArrayAvanced/concatenarArray.js
+++ JAVASCRIPT/ArrayAvanced/concatenarArray.js
@@ -1,15 +1,23 @@
 /*const numeros1 = [1, 2, 3]
 const numeros2 = [4, 5, 6]
 
-const conjunto = numeros1.concat(numeros2)
-console.log(conjunto)*/
+// agrupar array
+// 1º forma usando concat
+const conjunto = numeros1.concat(numeros2, [7,8,9])
+console.log(conjunto)
+
+// 2º forma usando ...rest
+// quando se pega o resto do array
+const numeros3 = [...numeros1, ...numeros2]
+console.log(numeros3)
+*/
 
 // FILTER
 /*
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
 const numerosFiltrados = numeros.filter(valor => valor > 10)
-console.log(numerosFiltradose)
+console.log(numerosFiltrados)
 */
 
 /*
@@ -22,19 +30,19 @@ const pessoas = [
 ]
 
 const pessoasNomeGrande = pessoas.filter(obj => obj.nome.length >= 6)
-const pessoasComMaisDeTrinta = pessoas.filter(obj => obj.idade >= 30)
-const nomeTerminaA = pessoas.filter(obj => {
-    return obj.nome.toLocaleLowerCase().endsWith('a')
+const pessoasComMaisDeTrinta = pessoas.filter(obj => obj.idade < 30)
+const nomeTerminaE = pessoas.filter(obj => {
+    return obj.nome.toLocaleLowerCase().endsWith('e')
 })
 
 console.log('1º ',pessoasNomeGrande)
 console.log('2º ',pessoasComMaisDeTrinta)
-console.log('3º ',nomeTerminaA)
+console.log('3º ',nomeTerminaE)
 */
 
 // MAP
 // Sempre vai ter o mesmo tamanho do Array original.
-
+/*
 const pessoas = [
     {nome: 'Maria', idade: 20},
     {nome: 'Maraje', idade: 21},
@@ -43,15 +51,19 @@ const pessoas = [
     {nome: 'Paula', idade: 35},
 ]
 
+// retorno de apenas nomes
 const nomes = pessoas.map(obj => obj.nome)
+
+// retorno de apenas idades
 const idades = pessoas.map(obj => ({idade: obj.idade}))
 
+// retorno do array com os ids
 const comIds = pessoas.map(function(obj, indice) {
     const newObj = { ...obj }
-    newObj.id = indice
+    newObj.id = indice //(indice + 1) * 1000
     return newObj
 })
 console.log(comIds)
-
+*/
 
 // REDUCE

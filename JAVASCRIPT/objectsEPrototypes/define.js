85117368e9579fe4067b641499df88f6ad361686
Mateus Rocha <mateus046@gmail.com>
2022-08-10 15:53:32 -0300

    define update

--- JAVASCRIPT/objectsEPrototypes/define.js
+++ JAVASCRIPT/objectsEPrototypes/define.js
@@ -1,5 +1,5 @@
-// defineProperty (uma chave) - defineProperties (várias chaves)
-
+// defineProperty (uma chave) -> Getters e Setters - defineProperties (várias chaves)
+/*
 function Produto(nome, preco, estoque) {
     this.nome = nome
     this.preco = preco
@@ -21,3 +21,34 @@ console.log(Object.keys(p1))
 for(let chave in p1) {
     console.log(chave)
 }
+*/
+
+// GETTERS E Setters
+function Produto(nome, preco, estoque) {
+    this.nome = nome
+    this.preco = preco
+
+    let estoquePriv = estoque
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // mostra a chave.
+        configurable: true, // reconfigurável ou não.
+        get: function () { 
+            return estoquePriv
+        },
+        set: function (valor) {
+            /*if(typeof valor !== 'number') {
+                console.log('bad')
+                return
+            }*/
+            if(typeof valor !== 'number') {
+                throw new TypeError('error')
+            }
+            estoquePriv = valor
+        }
+    })
+}
+
+const p1 = new Produto('Camiseta', 20, 3)
+p1.estoque = 500
+console.log(p1.estoque)

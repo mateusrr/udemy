e0c1ca133deba1b14aafe4cb6a110686e5caebad
Mateus Rocha <mateus046@gmail.com>
2022-08-25 10:39:14 -0300

    update coment

--- JAVASCRIPT/poliformismo.js
+++ JAVASCRIPT/poliformismo.js
@@ -1,3 +1,6 @@
+//Criando uma Conta para especializar a mesma em Corrente e Poupança
+//Contendo parâmetros de Agencia, Conta, Saldo
+//Além de Limite para a Corrente e Poupança
 function Conta(agencia, conta, saldo) {
     this.agencia = agencia
     this.conta = conta
@@ -5,11 +8,12 @@ function Conta(agencia, conta, saldo) {
 }
 
 Conta.prototype.sacar = function(valor) {
+    //Checando se tem saldo na conta.
     if(valor > this.saldo) {
         console.log(`Saldo insuficiente: ${this.saldo}`)
         return
     }
-
+    
     this.saldo -= valor
     this.verSaldo()
 }
@@ -24,7 +28,10 @@ Conta.prototype.verSaldo = function() {
 }
 
 const conta1 = new Conta(01, 07, 12)
-conta1.depositar(1)
+const conta2 = new Conta(02, 08, 500)
+/*conta1.depositar(200)
+conta2.sacar(300)
+console.log(conta1,'-', conta2)*/
 
 function ContaCorrente(agencia, conta, saldo, limite) {
     Conta.call(this, agencia, conta, saldo)
@@ -53,3 +60,4 @@ ContaPoupanca.prototype = Object.create(Conta.prototype)
 ContaPoupanca.prototype.constructor = ContaPoupanca
 
 const corrente = new ContaCorrente(11, 22, 0, 100)
+const poupanca = new ContaPoupanca(30, 01, 700, 150)

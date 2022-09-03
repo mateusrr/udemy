1dddcc94a7e5f577636ef956081d3cf93f98bcd5
Mateus Rocha <mateus046@gmail.com>
2022-09-03 12:37:38 -0300

    update promises

--- JAVASCRIPT/assincrono/promises.js
+++ JAVASCRIPT/assincrono/promises.js
@@ -12,9 +12,13 @@ function espera(msg, tempo) {
     //resolve - esse código exceutou com sucesso 
     //reject - deu erro no código para ser rejeitado.
     return new Promise((resolve, reject) => {
-        if(typeof msg !== 'string') reject('ERROR')
+        if(typeof msg !== 'string') {
+            reject('ERROR')
+            return
+        }
         setTimeout(() => {
-            resolve(msg) //só de fazer isso já está resolvendo logo após execução
+            resolve(msg.toUpperCase() + ' - passei na promise') //só de fazer isso já está resolvendo logo após execução
+            return
         }, tempo)
     })
 }
@@ -23,6 +27,7 @@ function espera(msg, tempo) {
 //reject / catch captura o erro
 // then / catch - podem receber funções
 //pode retornar uma promises dentro de outra.
+/*
 espera('Conectando...', rand(1, 3))
     .then(resposta => {
         console.log(resposta)
@@ -41,4 +46,66 @@ espera('Conectando...', rand(1, 3))
     })
     .catch(e => {
         console.log('Ops:', e)
+    })*/
+
+//métodos úteis
+// 1 promise.all - 2 promise.race - 3 promise.resolve - 4 promise.reject
+
+//promise.all - retorna uma promise que é resolvida quando todas as promises no
+//argumento lista forem resolvidas ou rejeitada assim que uma das promises da lista for
+//rejeitada.
+/* const promises = [
+    'primeiro valor',
+    espera('promise 1', 3000),
+    espera('promise 2', 400),
+    espera('promise 3', 1000),
+    //espera(100, 1000), // vai apresentar error por não ser string.
+    'outro valor'
+]
+Promise.all(promises)
+    .then(function(valor) {
+        console.log(valor)
     })
+    .catch(function(error) {
+        console.log(error)
+    })
+*/
+
+//promise.race
+//retorna a primeira que resolver.
+// 'primeiro valor' não é uma promessa, e sempre será entregue como primeiro.
+// precisa ser alterado, para que fique só as promessas.
+
+const promises = [
+    //'primeiro valor',
+    //RAND irá deixar como aleatório.
+    espera('promise 1', rand(1, 5)),
+    espera('promise 2', rand(1, 5)),
+    espera('promise 3', rand(1, 5)),
+    //espera(100, 1000), //vai apresentar error por não ser string.
+    //'outro valor'
+]
+
+Promise.race(promises)
+    .then(function(valor) {
+        console.log(valor)
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+
+function baixaPagina() {
+    const emCache = true
+
+    if(emCache) {
+        return Promise.resolve('Em cache.')
+    } else {
+        return espera('Baixei', 2000)
+    }
+}
+
+baixaPagina() 
+    .then(dadosPagina => {
+        console.log(dadosPagina)
+    })
+    .catch(e => console.log(e))

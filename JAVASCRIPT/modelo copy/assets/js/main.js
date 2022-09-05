56bb8211e07d43a76e327b6949d2cce6cb9eed68
Mateus Rocha <mateus046@gmail.com>
2022-09-05 12:18:51 -0300

    xml

---
+++ JAVASCRIPT/modelo copy/assets/js/main.js
@@ -0,0 +1,49 @@
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        //utilizar verbo http, chamado também de método... "GET"
+        //padrão do navegador
+        //async (true) or sync
+        xhr.open(obj.method, obj.url, true)
+        xhr.send() //se fosse feito com POST teria que mandar os dados.
+
+        xhr.addEventListener('load', () => {
+            if(xhr.status >= 200 && xhr.status < 300) {
+            //callback de sucesso
+            obj.success(xhr.responseText)
+            } else {
+                obj.error(xhr.statusText)
+            }
+        })
+    })
+}
+
+document.addEventListener('click', e => {
+    const el = e.target
+    const tag = el.tagName.toLowerCase()
+
+    if(tag === 'a') {
+        e.preventDefault()
+        carregaPag(el)
+    }
+})
+
+function carregaPag(el) {
+    const href = el.getAttribute('href')
+    
+    const objConfig = {
+        method: 'GET',
+        url: href,
+        success(response) {
+            carregaResultado(response)
+        },
+        error(errorText) {
+            console.log(errorText)
+        }
+    }
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado')
+    resultado.innerHTML = response
+}
